f2c6bcadb62642d6342416e2670a07ee92da9aae
ThalissonMelo <tallisonmelo46@gmail.com>
2019-09-25 18:59:11 -0300

    Adds member show component

---
+++ profile_crawler_front/src/components/ShowMember.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="q-pa-md q-gutter-sm">
+        <q-banner inline-actions rounded class="bg-blue text-white">
+            <p> Nome: {{memberData.name}} </p>
+            <p> Twitter URL: {{memberData.shortened_url}} </p>
+            <p> Twitter username: {{memberData.twitter_username}} </p>
+            <p> Twitter Description: {{memberData.twitter_description}} </p>
+        </q-banner>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      memberData: {}
+    }
+  },
+
+  created () {
+    const id = this.$route.params.id
+    this.getMember(id)
+  },
+
+  methods: {
+      getMember (id) {
+        const searchUrl = `http://localhost:3000/members/${id}`
+        axios
+          .get(searchUrl)
+          .then((res) => {
+            this.memberData = res.data
+          })
+          .catch((err) => console.log(err))
+      },
+    
+    twitterRedirect: function (url) {
+       window.open(url, "_blank");    
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
